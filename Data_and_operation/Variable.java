33fa51e841cc75683c525079f690193ce10185ef
ChoiJuneSeo <cjs07310125@gmail.com>
2023-09-17 20:49:55 +0900

    Casting 까지 복습

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,20 @@
+
+public class Variable 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 1;		// Number -> Integer
+		System.out.println(a);
+		
+		double b = 1.1;		// real number ... -1.0  , 0 , 1.0 ...
+		System.out.println(b);
+		
+		String c = "Hello World";
+		System.out.println(c);
+		
+		
+
+	}
+
+}
